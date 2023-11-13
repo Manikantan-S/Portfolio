064e13ff3e245cb6df66b454fbc78ff1bc6a11ba
P KARTHIK MANIKANTAN <111214511+Manikantan-S@users.noreply.github.com>
2023-11-13 21:32:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import {useEffect} from 'react'
+import Navbar from './components/Navbar';
+import Hiro from './components/Hiro';
+import Skills from './components/Skills';
+import Honors from './components/Honors';
+import Github from './components/github'
+import Certs from './components/Certs';
+import Footer from './components/Footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
+function App() {
+    useEffect(() => {
+      document.title = 'P Karthik';
+      AOS.init();
+    }, []);
+  return (
+    <div className="px-6 lg:px-20 xl:px-36 bg-dark-500">
+      <Navbar />
+      <Hiro />
+      <Skills />
+      <Honors />
+      <Github />
+      <Certs />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
